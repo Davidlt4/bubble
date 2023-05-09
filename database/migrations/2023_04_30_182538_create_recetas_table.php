d94b52b047eaa0bf3e7e40e1837501f9d15bbe29
Davidlt4 <lopez21tap@gmail.com>
2023-05-09 19:59:07 +0200

    segundo commit

---
+++ database/migrations/2023_04_30_182538_create_recetas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recetas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('ingredientes')->default(null);
+            $table->integer('id_imagen');
+            $table->integer('id_categoria');
+            $table->longText('preparacion');
+            $table->integer('id_usuario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recetas');
+    }
+};
